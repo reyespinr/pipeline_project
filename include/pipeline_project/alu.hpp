e4f7a74f0fe9e323f30165e0d000e89c0315c780
Nathan Reyes <reyespinr@vcu.edu>
2023-10-14 13:14:31 -0400

    added test and logic for alu flags

--- include/pipeline_project/alu.hpp
+++ include/pipeline_project/alu.hpp
@@ -3,7 +3,7 @@
 #include <bitset>
 #include <cstdint>
 
-class ALU32Bit
+class ALU
 {
 private:
   uint32_t a;
@@ -14,9 +14,7 @@ private:
   bool carry_out_flag;
 
 public:
-  ALU32Bit() : a(0), b(0), alu_ctr(0), zero_flag(false), overflow_flag(false), carry_out_flag(false)
-  {
-  }
+  ALU() : a(0), b(0), alu_ctr(0), zero_flag(false), overflow_flag(false), carry_out_flag(false) {}
 
   auto setInputs(uint32_t a_, uint32_t b_, std::bitset<3> alu_ctr_) -> void
   {
@@ -25,15 +23,23 @@ public:
     alu_ctr = alu_ctr_;
   }
 
-auto performOperation() -> uint32_t
-{
+  auto performOperation() -> uint32_t
+  {
     uint32_t result = 0;
     switch (alu_ctr.to_ulong()) {
       case 0b000:  // Addition
         result = a + b;
+        // Overflow check for addition
+        overflow_flag =
+          ((a & 0x80000000) == (b & 0x80000000)) && ((result & 0x80000000) != (a & 0x80000000));
+        carry_out_flag = result < a;
         break;
       case 0b001:  // Subtraction
         result = a - b;
+        // Overflow check for subtraction
+        overflow_flag =
+          ((a & 0x80000000) != (b & 0x80000000)) && ((result & 0x80000000) == (b & 0x80000000));
+        carry_out_flag = a < b;
         break;
       case 0b010:  // Bitwise AND
         result = a & b;
@@ -41,14 +47,14 @@ auto performOperation() -> uint32_t
       case 0b011:  // Bitwise OR
         result = a | b;
         break;
-      case 0b100:  // Logical left shift
-        result = a << b; // Shift by the value of b
+      case 0b100:         // Logical left shift
+        result = a << b;  // Shift by the value of b
         break;
-      case 0b101:  // Logical right shift
-        result = a >> b; // Shift by the value of b
+      case 0b101:         // Logical right shift
+        result = a >> b;  // Shift by the value of b
         break;
-      case 0b110:  // Arithmetic left shift (similar to logical left shift)
-        result = a << b; // Shift by the value of b
+      case 0b110:         // Arithmetic left shift (similar to logical left shift)
+        result = a << b;  // Shift by the value of b
         break;
       case 0b111:              // Arithmetic right shift
         if (a & 0x80000000) {  // If MSB (sign bit) is 1, it's negative
@@ -63,14 +69,10 @@ auto performOperation() -> uint32_t
         break;
     }
 
-    // Update flags
+    // Update the zero flag
     zero_flag = (result == 0);
-    overflow_flag =
-      false;  // Simple check. More complicated operations will need a more robust check.
-    carry_out_flag = false;  // Placeholder for now, need to handle in addition/subtraction.
-
     return result;
-}
+  }
 
   auto isZero() const -> bool { return zero_flag; }
   auto hasOverflow() const -> bool { return overflow_flag; }
--- test/test_alu.cpp
+++ test/test_alu.cpp
@@ -4,141 +4,201 @@
 
 TEST(TestALU, Addition)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(5, 7, std::bitset<3>(0b000));  // Addition
   ASSERT_EQ(alu.performOperation(), 12);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, Subtraction)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(7, 5, std::bitset<3>(0b001));  // Subtraction
   ASSERT_EQ(alu.performOperation(), 2);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, BitwiseAND)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0b1100, 0b1010, std::bitset<3>(0b010));  // Bitwise AND
   ASSERT_EQ(alu.performOperation(), 0b1000);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, BitwiseOR)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0b1100, 0b1010, std::bitset<3>(0b011));  // Bitwise OR
   ASSERT_EQ(alu.performOperation(), 0b1110);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, LogicalLeftShift)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0b1100, 0b1, std::bitset<3>(0b100));  // Logical left shift
   ASSERT_EQ(alu.performOperation(), 0b11000);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, LogicalRightShift)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0b1100, 0b1, std::bitset<3>(0b101));  // Logical right shift
   ASSERT_EQ(alu.performOperation(), 0b110);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, ArithmeticLeftShift)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0b1100, 0b1, std::bitset<3>(0b110));  // Arithmetic left shift
   ASSERT_EQ(alu.performOperation(), 0b11000);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, ArithmeticRightShiftPositive)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0b1100, 0b1, std::bitset<3>(0b111));  // Arithmetic right shift
   ASSERT_EQ(alu.performOperation(), 0b110);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, ArithmeticRightShiftNegative)
 {
-  ALU32Bit alu;
+  ALU alu;
   uint32_t negativeValue = 0xFFFFFFFF;                     // Represents -1 in two's complement
   alu.setInputs(negativeValue, 0, std::bitset<3>(0b111));  // Arithmetic right shift
   ASSERT_EQ(alu.performOperation(), negativeValue);  // Should remain negative (MSB remains set)
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, AdditionWithOverflow)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0x80000000, 0x80000000, std::bitset<3>("000"));
   ASSERT_EQ(alu.performOperation(), 0x00000000);
+  ASSERT_TRUE(alu.isZero());
+  ASSERT_TRUE(alu.hasOverflow());
+  ASSERT_TRUE(alu.hasCarryOut());
 }
 
 TEST(TestALU, SubtractionWithUnderflow)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0x00000000, 0x00000001, std::bitset<3>("001"));
   ASSERT_EQ(alu.performOperation(), 0xFFFFFFFF);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_TRUE(alu.hasCarryOut());
 }
 
 TEST(TestALU, BitwiseANDMixedBits)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0x00F0F0F0, 0xFF0F0F0F, std::bitset<3>("010"));
   ASSERT_EQ(alu.performOperation(), 0x00000000);
+  ASSERT_TRUE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, BitwiseORMixedBits)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0x00F0F0F0, 0xFF0F0F0F, std::bitset<3>("011"));
   ASSERT_EQ(alu.performOperation(), 0xFFFFFFFF);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, LogicalLeftShiftMax)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0x00000001, 31, std::bitset<3>("100"));
   ASSERT_EQ(alu.performOperation(), 0x80000000);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, LogicalRightShiftMax)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0x80000000, 31, std::bitset<3>("101"));
   ASSERT_EQ(alu.performOperation(), 0x00000001);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, ArithmeticLeftShiftNegative)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0xFFFFFFFF, 1, std::bitset<3>("110"));
   ASSERT_EQ(alu.performOperation(), 0xFFFFFFFE);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, ArithmeticRightShiftLargerNegative)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0x80FFFFFF, 1, std::bitset<3>("111"));
   ASSERT_EQ(alu.performOperation(), 0xC07FFFFF);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, ArithmeticRightShiftLargerPositive)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0x7FFFFFFF, 1, std::bitset<3>("111"));
   ASSERT_EQ(alu.performOperation(), 0x3FFFFFFF);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, BitwiseANDRandomValues)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0x55555555, 0xAAAAAAAA, std::bitset<3>("010"));
   ASSERT_EQ(alu.performOperation(), 0x00000000);
+  ASSERT_TRUE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
 
 TEST(TestALU, BitwiseORRandomValues)
 {
-  ALU32Bit alu;
+  ALU alu;
   alu.setInputs(0x55555555, 0xAAAAAAAA, std::bitset<3>("011"));
   ASSERT_EQ(alu.performOperation(), 0xFFFFFFFF);
+  ASSERT_FALSE(alu.isZero());
+  ASSERT_FALSE(alu.hasOverflow());
+  ASSERT_FALSE(alu.hasCarryOut());
 }
