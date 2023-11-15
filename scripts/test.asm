0bad2d58ce799ec2507f405380e9fe1f6ce94861
Nathan Reyes <reyespinr@vcu.edu>
2023-11-14 19:43:47 -0500

    added demo files

--- scripts/test.asm
+++ scripts/test.asm
@@ -1,12 +1,43 @@
-# R-Type Instructions
-add $1, $2, $3    # Adds contents of $2 and $3, result in $1
-sub $4, $5, $6    # Subtracts $5 from $6, result in $4
+# Initialize registers
+li $v0, 0x0040       # $v0 = 0040hex
+li $v1, 0x1010       # $v1 = 1010hex
+li $s2, 0x000F       # $s2 = 000Fhex
+li $s3, 0x00F0       # $s3 = 00F0hex
+li $t0, 0x0000       # $t0 = 0000hex
+li $a0, 0x0010       # $a0 = 0010hex
+li $a1, 0x0005       # $a1 = 0005hex
 
-# I-Type Instructions
-addi $8, $9, 10   # Adds immediate value 10 to contents of $9, result in $8
-lw   $10, 4($11)  # Load word from memory address in $11 + offset 4 into $10
-sw   $12, 8($13)  # Store word in $12 to memory address in $13 + offset 8
-beq  $14, $15, 16 # Branch to instruction 16 if $14 and $15 are equal
+loop:                # While loop start
+    bgtz $a1, loop_body  # Branch if $a1 > 0
+    j end_loop           # Jump to end of loop if $a1 <= 0
 
-# J-Type Instruction
-j 1024            # Jump to instruction at address 1024
+loop_body:
+    addi $a1, $a1, -1    # $a1 = $a1 - 1
+    lw $t0, 0($a0)       # $t0 = Mem[$a0]
+
+    # Check if $t0 > 0100hex
+    li $t1, 0x0100
+    ble $t0, $t1, else_block   # If $t0 <= 0x0100, jump to else block
+
+    # If block
+    srl $v0, $v0, 3      # $v0 = $v0 / 8
+    or $v1, $v1, $v0     # $v1 = $v1 | $v0
+    li $t2, 0xFF00
+    sw $t2, 0($a0)       # Mem[$a0] = FF00hex
+    j continue_loop
+
+else_block:
+    # Else block
+    sll $s2, $s2, 2      # $s2 = $s2 * 4
+    xor $s3, $s3, $s2    # $s3 = $s3 ^ $s2
+    li $t2, 0x00FF
+    sw $t2, 0($a0)       # Mem[$a0] = 00FFhex
+
+continue_loop:
+    addi $a0, $a0, 2     # $a0 = $a0 + 2
+    j loop               # Jump back to the start of the loop
+
+end_loop:
+    # The return statement is usually a jump to the return address in MIPS,
+    # but in this context, it's just the end of the program
+    # jr $ra             # Normally, return to the calling function
